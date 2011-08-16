819673d15f9e6fc7cb1f801d377cafa50f3c5e9d
john@andersoninnovative.com <john@andersoninnovative.com@41e50629-eb6e-b841-421f-ad03d4eac035>
2011-08-16 14:00:54 +0000

    Simplified URLs, views and templates; added validation stub module.

---
+++ branches/google-apps/idptest/saml2idp/validation.py
@@ -0,0 +1,9 @@
+"""
+Validations for various conditions; or place-holders for future enhancement.
+These methods should return nothing for success and raise an exception on
+invalid conditions. (I think.)
+"""
+
+def validate_request(authn_req):
+    #XXX: Validate against known/approved SPs?
+    return
