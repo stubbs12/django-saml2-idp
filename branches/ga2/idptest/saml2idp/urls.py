5911b1ad6f6359bd62aefeddb0c82572cf2a5f9f
john@andersoninnovative.com <john@andersoninnovative.com@41e50629-eb6e-b841-421f-ad03d4eac035>
2011-09-22 20:11:56 +0000

    Simplified login URL.

--- branches/ga2/idptest/saml2idp/urls.py
+++ branches/ga2/idptest/saml2idp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from views import login_begin, login_process, logout
 
 urlpatterns = patterns('',
-   url( r'^login/begin/$', login_begin, name="login_begin"),
+   url( r'^login/$', login_begin, name="login_begin"),
    url( r'^login/process/$', login_process, name='login_process'),
    url( r'^logout/$', logout, name="logout"),
 )
