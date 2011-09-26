965d49bc2fe4bbfeea572e1cfce8024f0f5128e6
john@andersoninnovative.com <john@andersoninnovative.com@41e50629-eb6e-b841-421f-ad03d4eac035>
2011-09-26 13:30:33 +0000

    Added stub classes for views tests.

---
+++ branches/ga2/idptest/saml2idp/tests/views.py
@@ -0,0 +1,17 @@
+"""
+Tests for basic view functionality only.
+"""
+from django.test import TestCase
+from django.test.client import Client
+
+
+class TestLoginView(TestCase):
+    pass
+
+
+class TestLoginProcessView(TestCase):
+    pass
+
+
+class TestLogoutView(TestCase):
+    pass
