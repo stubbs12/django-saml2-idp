7ae17226b8cf4d1c24560b4d47d1e394f9291d48
john@andersoninnovative.com <john@andersoninnovative.com@41e50629-eb6e-b841-421f-ad03d4eac035>
2011-09-26 13:44:36 +0000

    Added test_logout_user() testcase.

--- branches/ga2/idptest/saml2idp/tests/views.py
+++ branches/ga2/idptest/saml2idp/tests/views.py
@@ -1,6 +1,7 @@
 """
 Tests for basic view functionality only.
 """
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test.client import Client
 
@@ -15,5 +16,18 @@ class TestLoginProcessView(TestCase):
 
 class TestLogoutView(TestCase):
     def test_logout(self):
+        """
+        Response did not say logged out.
+        """
         response = self.client.get('/idp/logout/')
         self.assertContains(response, 'logged out', status_code=200)
+
+    def test_logout_user(self):
+        """
+        User account not logged out.
+        """
+        fred = User.objects.create_user('fred', email='fred@example.com', password='secret')
+        self.client.login(username='fred', password='secret')
+        self.assertTrue('_auth_user_id' in self.client.session, 'Did not login test user; test is broken.')
+        response = self.client.get('/idp/logout/')
+        self.assertTrue('_auth_user_id' not in self.client.session, 'Did not logout test user.')
