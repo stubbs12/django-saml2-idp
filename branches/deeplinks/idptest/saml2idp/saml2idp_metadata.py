dfd95661881be91257aa97a9fb3b7accf5d6f1a3
john@andersoninnovative.com <john@andersoninnovative.com@41e50629-eb6e-b841-421f-ad03d4eac035>
2012-06-26 12:49:44 +0000

    Don't supply partial default settings; only supply settings when none are specified, so that this works out-of-the-box (and as an example).

--- branches/deeplinks/idptest/saml2idp/saml2idp_metadata.py
+++ branches/deeplinks/idptest/saml2idp/saml2idp_metadata.py
@@ -6,53 +6,41 @@ Detailed discussion is in doc/SETTINGS_AND_METADATA.txt.
 __all__ = [ 'SAML2IDP_CONFIG', 'SAML2IDP_REMOTES' ]
 from django.conf import settings
 
-###################
-# Setup defaults. #
-###################
-SAML2IDP_CONFIG = {
-    # Default metadata to configure this local IdP.
-    'autosubmit': True,
-    'certificate_file': 'keys/certificate.pem', # If using relative paths, be careful!
-    'private_key_file': 'keys/private-key.pem', # If using relative paths, be careful!
-    'issuer': 'http://127.0.0.1:8000',
-    'signing': True,
-}
-
-# To test against the companion project, use these settings:
-demoSpConfig = {
-    'acs_url': 'http://127.0.0.1:9000/sp/acs/',
-    'processor': 'saml2idp.demo.Processor',
-    'links': {
-        'deeplink': 'http://127.0.0.1:9000/sp/%s/',
-    }
-}
-attrSpConfig = {
-    'acs_url': 'http://127.0.0.1:9000/sp/acs/',
-    'processor': 'saml2idp.demo.AttributeProcessor',
-    'links': {
-        'attr': 'http://127.0.0.1:9000/sp/%s/',
-    },
-}
-
-SAML2IDP_REMOTES = {
-    # Group of SP CONFIGs.
-    # friendlyname: SP config
-    'attr_demo': attrSpConfig,
-    'demo': demoSpConfig,
-    # 'salesforce': salesforceSpConfig,
-}
-
-############################################
-# Pull in overrides from project settings. #
-############################################
 try:
-    overrides = settings.SAML2IDP_CONFIG
+    SAML2IDP_CONFIG = settings.SAML2IDP_CONFIG
     SAML2IDP_CONFIG.update(overrides)
 except:
-    pass
+    # Provide settings so that the demo will work out-of-the-box.
+    SAML2IDP_CONFIG = {
+        # Default metadata to configure this local IdP.
+        'autosubmit': True,
+        'certificate_file': '%s/keys/sample/sample-certificate.pem' % settings.PROJECT_ROOT,
+        'private_key_file': '%s/keys/sample/sample-private-key.pem' % settings.PROJECT_ROOT,
+        'issuer': 'http://127.0.0.1:8000',
+        'signing': True,
+    }
 
 try:
-    remotes = settings.SAML2IDP_REMOTES
-    SAML2IDP_REMOTES.update(remotes)
+    SAML2IDP_REMOTES = settings.SAML2IDP_REMOTES
 except:
-    pass
+    # Provide settings so that the demo will work out-of-the-box.
+    demoSpConfig = {
+        'acs_url': 'http://127.0.0.1:9000/sp/acs/',
+        'processor': 'saml2idp.demo.Processor',
+        'links': {
+            'deeplink': 'http://127.0.0.1:9000/sp/%s/',
+        }
+    }
+    attrSpConfig = {
+        'acs_url': 'http://127.0.0.1:9000/sp/acs/',
+        'processor': 'saml2idp.demo.AttributeProcessor',
+        'links': {
+            'attr': 'http://127.0.0.1:9000/sp/%s/',
+        },
+    }
+    SAML2IDP_REMOTES = {
+        # Group of SP CONFIGs.
+        # friendlyname: SP config
+        'attr_demo': attrSpConfig,
+        'demo': demoSpConfig,
+    }
